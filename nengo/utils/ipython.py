a8d25051663bf73dde8da60c978b2358a9de73ed
Trevor Bekolay <tbekolay@gmail.com>
2018-05-20 11:48:16 -0400

    Test upgraded versions

--- nengo/utils/ipython.py
+++ nengo/utils/ipython.py
@@ -1,40 +1,9 @@
-"""Functions for easy interactions with IPython and IPython notebooks.
-
-NotebookRunner is modified from runipy.
-This modified code is included under the terms of its license:
-
-Copyright (c) 2013, Paul Butler
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
+"""Functions for easy interactions with IPython and IPython notebooks."""
 
 from __future__ import absolute_import
 
 import io
 import os
-import platform
-import time
 import uuid
 
 import numpy as np
@@ -55,8 +24,6 @@ try:
     if IPython.version_info[0] <= 2:
         # pylint: disable=ungrouped-imports
         from IPython.nbformat import current
-        from IPython.nbformat.current import write as write_nb
-        from IPython.nbformat.current import NotebookNode
 
         def read_nb(fp):
             return current.read(fp, 'json')
@@ -64,12 +31,8 @@ try:
         if IPython.version_info[0] == 3:
             # pylint: disable=ungrouped-imports
             from IPython import nbformat
-            from IPython.nbformat import write as write_nb
-            from IPython.nbformat import NotebookNode
         else:
             import nbformat
-            from nbformat import write as write_nb
-            from nbformat import NotebookNode
 
         def read_nb(fp):
             # Have to load as version 4 or running notebook fails
@@ -278,154 +241,14 @@ def export_images(resources, image_dir, image_rel_dir, html_out):
     return html_out
 
 
-def export_evaluated(nb, dest_path=None, skip_exceptions=False):
-    """Convert notebook to an evaluated notebook.
+def iter_cells(nb, cell_type="code"):
+    if nb.nbformat <= 3:
+        cells = []
+        for ws in nb.worksheets:
+            cells.extend(ws.cells)
+    else:
+        cells = nb.cells
 
-    Optionally saves the notebook to dest_path.
-    """
-    nb_runner = NotebookRunner(nb)
-    nb_runner.run_notebook(skip_exceptions=skip_exceptions)
-
-    if dest_path is not None:
-        with io.open(dest_path, 'w', encoding='utf-8') as f:
-            if IPython.version_info[0] <= 2:
-                write_nb(nb_runner.nb, f, 'ipynb')
-            else:
-                write_nb(nb_runner.nb, f)
-    return nb_runner.nb
-
-
-class NotebookRunner(object):
-    # The kernel communicates with mime-types while the notebook
-    # uses short labels for different cell types. We'll use this to
-    # map from kernel types to notebook format types.
-
-    MIME_MAP = {
-        'image/jpeg': 'jpeg',
-        'image/png': 'png',
-        'text/plain': 'text',
-        'text/html': 'html',
-        'text/latex': 'latex',
-        'application/javascript': 'html',
-        'image/svg+xml': 'svg',
-    }
-
-    def __init__(self, nb, working_dir=None):
-        from IPython.kernel import KernelManager
-
-        self.km = KernelManager()
-
-        cwd = os.getcwd()
-        if working_dir is not None:
-            os.chdir(working_dir)
-        self.km.start_kernel()
-        os.chdir(cwd)
-
-        if platform.system() == 'Darwin':
-            # There is sometimes a race condition where the first
-            # execute command hits the kernel before it's ready.
-            # It appears to happen only on Darwin (Mac OS) and an
-            # easy (but clumsy) way to mitigate it is to sleep
-            # for a second.
-            time.sleep(1)
-
-        self.kc = self.km.client()
-        self.kc.start_channels()
-        self.shell = self.kc.shell_channel
-        self.iopub = self.kc.iopub_channel
-        self.nb = nb
-
-    def __del__(self):
-        self.kc.stop_channels()
-        self.km.shutdown_kernel(now=True)
-
-    def run_cell(self, cell):  # noqa: C901
-        """Run a notebook cell and update the output of that cell in-place."""
-        self.shell.execute(cell.input)
-        reply = self.shell.get_msg()
-        status = reply['content']['status']
-        if status == 'error':
-            traceback_text = ("Cell raised uncaught exception: \n"
-                              "\n".join(reply['content']['traceback']))
-
-        outs = []
-        while True:
-            msg = self.iopub.get_msg(timeout=1)
-            msg_type = msg['msg_type']
-            content = msg['content']
-
-            if msg_type == 'status' and content['execution_state'] == 'idle':
-                break
-
-            # IPython 3.0.0-dev writes pyerr/pyout in the notebook format
-            # but uses error/execute_result in the message spec. This does
-            # the translation needed for tests to pass with IPython 3.0.0-dev
-            notebook3_format_conversions = {
-                'error': 'pyerr',
-                'execute_result': 'pyout',
-            }
-            msg_type = notebook3_format_conversions.get(msg_type, msg_type)
-
-            out = NotebookNode(output_type=msg_type)
-
-            if 'execution_count' in content:
-                cell['prompt_number'] = content['execution_count']
-                out.prompt_number = content['execution_count']
-
-            if msg_type in ('status', 'pyin', 'execute_input',
-                            'comm_open', 'comm_msg'):
-                continue
-            elif msg_type == 'stream':
-                out.stream = content['name']
-                out.text = content['data']
-            elif msg_type in ('display_data', 'pyout'):
-                for mime, data in content['data'].items():
-                    try:
-                        attr = self.MIME_MAP[mime]
-                    except KeyError:
-                        raise NotImplementedError(
-                            'unhandled mime type: %s' % mime)
-                    if "widgets/js/widget" in data:
-                        continue
-                    setattr(out, attr, data)
-            elif msg_type == 'pyerr':
-                out.ename = content['ename']
-                out.evalue = content['evalue']
-                out.traceback = content['traceback']
-            elif msg_type == 'clear_output':
-                outs = []
-                continue
-            else:
-                raise NotImplementedError(
-                    'unhandled iopub message: %s' % msg_type)
-            outs.append(out)
-        cell['outputs'] = outs
-
-        if status == 'error':
-            raise Exception(traceback_text)
-
-    def iter_code_cells(self):
-        """Iterate over the notebook cells containing code."""
-        for ws in self.nb.worksheets:
-            for cell in ws.cells:
-                if cell.cell_type == 'code':
-                    yield cell
-
-    def run_notebook(self, skip_exceptions=False, progress_callback=None):
-        """Runs all notebook cells in order and updates outputs in-place.
-
-        If ``skip_exceptions`` is True, then if exceptions occur in a cell, the
-        subsequent cells are run (by default, the notebook execution stops).
-        """
-        for i, cell in enumerate(self.iter_code_cells()):
-            try:
-                self.run_cell(cell)
-            except Exception:
-                if not skip_exceptions:
-                    raise
-            if progress_callback is not None:
-                progress_callback(i)
-
-    def count_code_cells(self):
-        """Return the number of code cells in the notebook."""
-        return sum(1 for _ in self.iter_code_cells())
+    for cell in cells:
+        if cell.cell_type == cell_type:
+            yield cell
